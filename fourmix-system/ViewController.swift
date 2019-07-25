dcd9a614e019bb38926579174f0f8c2ca93fb2a6
lindelea <kgo@lindelin.org>
2019-07-25 11:27:03 +0900

    Initial Commit

---
+++ fourmix-system/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  fourmix-system
+//
+//  Created by Jie Wu on 2019/07/25.
+//  Copyright © 2019 Fourmix. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
