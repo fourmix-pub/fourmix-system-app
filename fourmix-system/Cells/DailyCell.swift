2068fc424a589de6bc9ff450cfbee2bd5cc0d87e
hishihara <ishrhnn19@gmail.com>
2019-07-30 11:30:53 +0900

    デイリーセルとコントローラーを作成

---
+++ fourmix-system/Cells/DailyCell.swift
@@ -0,0 +1,41 @@
+//
+//  DailyCell.swift
+//  fourmix-system
+//
+//  Created by 石原比希 on 2019/07/30.
+//  Copyright © 2019 Fourmix. All rights reserved.
+//
+
+import UIKit
+
+class DailyCell: UITableViewCell {
+    
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var projectNameLabel: UILabel!
+    @IBOutlet weak var workTypeLabel: UILabel!
+    @IBOutlet weak var workTimeLabel: UILabel!
+    @IBOutlet weak var restTimeLabel: UILabel!
+    @IBOutlet weak var jobTypeLabel: UILabel!
+    @IBOutlet weak var noteLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    func update(daily: Daily) {
+        dateLabel.text = daily.attributes.date
+        projectNameLabel.text = daily.relationships.project.attributes.name
+        workTypeLabel.text = daily.relationships.workType.attributes.name
+        workTimeLabel.text = "\(daily.attributes.start)~\(daily.attributes.end)"
+        restTimeLabel.text = "\(daily.attributes.rest ?? 0)分"
+        jobTypeLabel.text = daily.relationships.jobType.attributes.name
+        noteLabel.text = daily.attributes.note
+    }
+}
---
+++ fourmix-system/Controllers/DailyController.swift
@@ -0,0 +1,114 @@
+//
+//  DailyController.swift
+//  fourmix-system
+//
+//  Created by 石原比希 on 2019/07/30.
+//  Copyright © 2019 Fourmix. All rights reserved.
+//
+
+import UIKit
+
+class DailyController: UITableViewController {
+
+    var dailies: [Daily] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        refreshControl = UIRefreshControl()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    @objc func loadDate() {
+        
+    }
+    
+    // MARK: - Table view data source
+    
+    // セクションの数
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    // セクションの行の数
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return dailies.count
+    }
+
+    // セルの指定
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "DailyCell", for: indexPath) as! DailyCell
+
+        cell.update(daily: dailies[indexPath.row])
+
+        return cell
+    }
+
+    /*
+    // 日報をスワイプした時のアクション
+    override func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        // 編集
+        let editAction = UIContextualAction(style: .normal, title: "編集") { (_, _, _) in
+            <#code#>
+        }
+        // 削除
+        let deleteAction = UIContextualAction(style: .normal, title: "削除") { (_, _, _) in
+            let dailyCreator = DailyCreator(id: self.dailies[indexPath.row].id, workTypeId: nil, jobTypeId: nil, projectId: nil, date: nil, start: nil, end: nil, rest: nil, note: nil)
+            
+        }
+    }
+     */
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
